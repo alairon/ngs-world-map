5ab9de66a29b4e717f34084b948957ea511a3d45
Ambrose Lai <alairon@outlook.com>
2021-05-24 14:25:33 -0400

    Redirects from the base page to the NGS map

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const { getRedirectStatus } = require("next/dist/lib/load-custom-routes");
+
 module.exports = {
   future: {
     webpack5: true
+  },
+
+  async redirects(){
+    return([{
+      source: '/',
+      destination: '/ngs/map',
+      permanent: true,
+    }]);
   }
 }
