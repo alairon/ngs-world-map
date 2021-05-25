6e9deff2df87639dedbfe62e59b1a5c9c1668c41
Ambrose Lai <alairon@outlook.com>
2021-05-25 00:43:15 -0400

    Added Next's getStaticProps for markers

--- pages/ngs/map.tsx
+++ pages/ngs/map.tsx
@@ -1,7 +1,21 @@
 import Head from 'next/head';
 import dynamic from "next/dynamic";
+import {getGatheringMarkers} from '../api/gathering';
+import { getLandmarkMarkers } from '../api/landmarks';
 
-export default function Map() {
+export async function getStaticProps(){
+  const landmarks = await getLandmarkMarkers('en');
+  const gathering = await getGatheringMarkers('en');
+
+  return ({
+    props: {
+      landmarks,
+      gathering
+    }
+  });
+}
+
+export default function Map(props) {
 
   // Imports the map with SSR disabled since it needs to be run on the client
   const MapView = dynamic(() => import ("../../components/map/map"), {
@@ -16,7 +30,7 @@ export default function Map() {
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
 
-      <MapView lang="en"/>
+      <MapView {...props}/>
     </main>
   );
 }
