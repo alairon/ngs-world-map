ba53a0769614b0633fcf8a0623b14bb7ffa7d677
Snowthorne <snowthorne@outlook.com>
2021-06-04 19:09:10 -0400

    Landmark DataStructRev L3

--- tools/condenseLandmarkData.js
+++ tools/condenseLandmarkData.js
@@ -1,4 +1,5 @@
 /* condenseLandmarkData.js
+  Uses data structure revision: L3
   This file attempts to gather the data from the /data/map/markers folder
   based on the language passed in (e.g. 'en', 'jp', etc.) and compresses them
   into a single JSON folder.\
@@ -7,32 +8,44 @@ const fs = require('fs');
 const path = require('path');
 
 function getMarkers(lang, continent){
-  const inputDir = path.join(__dirname, '../data/' + continent, 'landmarks');
+  const inputDir = path.join(__dirname, '../data/', `${continent}`, 'landmarks', `${lang}`);
+  const markerStrings = path.join(__dirname, '../data', `${continent}`, 'landmarks', `landmarks_${lang}.json`);
   const outputDir = path.join(__dirname, '../data/ngs/');
-  const markerDir = path.join(inputDir, '/');
   const jsonRegex = /.\.json/mi;
   const markers = [];
   
-  const files = fs.readdirSync(markerDir);
-
-  for (const file in files){
-    // Test if the current file is a JSON file
-    if (jsonRegex.test(files[file])){
-      // Reads the JSON file
-      let content = JSON.parse(fs.readFileSync(markerDir+files[file], 'utf8'));
-
-      // Reads each section of the JSON. See the skeleton.txt file in the data/map/_lang_ folder for details
-      for (const x in Object.keys(content)) {
-        let length = content[Object.keys(content)[x]].length;
-
-        if (length > 0){
-          for (let y = 0; y < length; y++){
-            markers.push(content[Object.keys(content)[x]][y]);
-          }
-        }
-      }
-    }
+  // Read the contents of the common marker file
+  let coreMarkerData = fs.readFileSync(markerStrings, 'utf8');
+  if (coreMarkerData && typeof(coreMarkerData) == 'string'){
+    coreMarkerData = JSON.parse(coreMarkerData);
   }
+
+  let fileList;
+  try {
+    fileList = fs.readdirSync(inputDir);
+  } catch (err) {
+    console.error("No file found");
+    fileList = [];
+  }
+
+  // Go through the list of files found in the inputDir
+  fileList.map((fileName) => {
+    // Test if the current file is a JSON file
+    if (jsonRegex.test(fileName)){
+      // Reads the JSON file
+      let content = JSON.parse(fs.readFileSync(path.join(inputDir, fileName), 'utf8'));
+
+      let keys = Object.keys(content);
+
+      keys.map((key) => {
+        if (content[key].length > 0){
+          content[key].map((data) => {
+            markers.push(Object.assign(coreMarkerData[key], data));
+          })
+        }
+      });
+    }
+  });
 
   // Attempt to write a new JSON file with the compiled data
   try{
